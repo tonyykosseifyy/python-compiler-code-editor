291b72be2a9e408c0224f9658755ae0754e68315
tonyy kosseifyy <tonykosseify123@gmail.com>
2022-12-03 20:13:43 +0200

    aam awe albe

--- app.py
+++ app.py
@@ -1,6 +1,57 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from tkinter.ttk import *
+import ctypes
+import re
+from main import * 
+
+# Increas Dots Per inch so it looks sharper
+ctypes.windll.shcore.SetProcessDpiAwareness(True)
+
+window = tk.Tk()
+window.title("TK Code Editor")
+
+window.rowconfigure(1, minsize=700, weight=1)
+window.columnconfigure(1, minsize=900, weight=1)
+
+style = Style()
+style.configure('TButton', font =
+               ('calibri', 16, 'bold'),
+                    borderwidth = '4')
+style.configure('TFrame', background='#2A2A2A')
+
+style.map('TButton')
+
+normal = "#EAEAEA"
+keywords = "#EA5F5F"
+comments = "#5FEAA5"
+string = "#EAA25F"
+variables = "#3681c6"
+background = "#2A2A2A"
+font = 'Consolas 15'
+
+
+s = {"a":"" , "b": ""}
+
+list = []
+
+for key in s :
+    list.append(key)
+txt = " | ".join(list)
+print(txt)
+errors = []
+
+
+editArea = tk.Text(
+    window,
+    background=background,
+    foreground=normal,
+    insertbackground=normal,
+    font=font,
+    padx=5,
+    pady=15
+)
+
 
 def open_file():
     filepath = askopenfilename(
@@ -8,10 +59,10 @@ def open_file():
     )
     if not filepath:
         return
-    txt_edit.delete("1.0", tk.END)
+    editArea.delete("1.0", tk.END)
     with open(filepath, mode="r", encoding="utf-8") as input_file:
         text = input_file.read()
-        txt_edit.insert(tk.END, text)
+        editArea.insert(tk.END, text)
     window.title(f"Simple Text Editor - {filepath}")
 
 def save_file():
@@ -22,40 +73,89 @@ def save_file():
     if not filepath:
         return
     with open(filepath, mode="w", encoding="utf-8") as output_file:
-        text = txt_edit.get("1.0", tk.END)
+        text = editArea.get("1.0", tk.END)
         output_file.write(text)
     window.title(f"Simple Text Editor - {filepath}")
 
-window = tk.Tk()
-window.title("TK Text Editor")
 
-window.rowconfigure(0, minsize=800, weight=1)
-window.columnconfigure(1, minsize=800, weight=1)
-
-style = Style()
- 
-style.configure('TButton', font =
-               ('calibri', 16, 'bold'),
-                    borderwidth = '4')
-style.configure('TFrame', background='#2A2A2A')
-
-style.map('TButton')
-
-txt_edit = tk.Text(window)
-frm_buttons = Frame(window, relief=tk.RAISED ,borderwidth=2, width=400 )
+frm_buttons = Frame(window,borderwidth=1, width=400 )
 btn_open = Button(frm_buttons, text="Open", command=open_file)
 btn_save = Button(frm_buttons, text="Save As...", command=save_file)
 btn_run = Button(frm_buttons, text="Run")
+output_area = tk.Frame(window, height=300, borderwidth=2 , relief=tk.RAISED, background="#2A2A2A")
+output_area.grid(row=1 , column=1,columnspan=2, sticky="nsew")
 
-##image = "image.png", compound=LEFT
+output_lbl = Label(output_area, text="Output" , foreground="white", background="#2A2A2A")
+output_lbl.pack(side="top")
 
 btn_open.grid(row=0, column=0, sticky="ew", padx=15, pady=30)
 btn_save.grid(row=1, column=0, sticky="ew", padx=15)
 btn_run.grid(row=2, column=0, sticky="ew", padx=15, pady=30,)
 
 
-frm_buttons.grid(row=0, column=0, sticky="ns")
-txt_edit.grid(row=0, column=1, sticky="nsew")
+frm_buttons.grid(row=0, column=0, sticky="ns", rowspan=2)
+editArea.grid(row=0, column=1, sticky="nsew")
+
+previousText = ''
+
+# Define a list of Regex Pattern that should be colored in a certain way
+repl = [
+    ['(^| )(False|None|True|and|as|assert|async|await|break|class|continue|def|del|elif|else|except|finally|for|from|global|if|import|in|is|lambda|nonlocal|not|or|pass|raise|return|try|while|with|yield)($| )', keywords],
+    ['".*?"', string],
+    ['\'.*?\'', string],
+    ['#.*?$', string],
+    [txt , variables ],
+    ['^\s*(\w+)\s*\((.*)\)\s*$' , comments]
+]
+
+
+# Insert some Standard Text into the Edit Area
+editArea.insert('1.0', """from argparse import ArgumentParser
+from random import shuffle, choice
+import string
+
+# Setting up the Argument Parser
+parser = ArgumentParser(
+
+    prog='Password Generator.',
+    description='Generate any number of passwords with this tool.'
+)
+""")
+
+
+def search_re(pattern, text):
+    matches = []
+    text = text.splitlines()
+    for i, line in enumerate(text):
+        for match in re.finditer(pattern, line):
+
+            matches.append(
+                (f"{i + 1}.{match.start()}", f"{i + 1}.{match.end()}")
+            )
+
+    return matches
+
+def changes(event=None):
+    for tag in editArea.tag_names():
+        editArea.tag_remove(tag, "1.0", "end")
+        # Add tags where the search_re variables found the pattern
+    i = 0
+    for pattern, color in repl:
+        for start, end in search_re(pattern, editArea.get('1.0', tk.END)):
+            editArea.tag_add(f'{i}', start, end)
+            editArea.tag_config(f'{i}', foreground=color)
+            i+=1
+
+
+def run() :
+    lines = editArea.get(1.0, tk.END)
+
+
+editArea.bind('<KeyRelease>', changes, run)
+
+
+changes()
+
 
 
 window.mainloop()
